e541e2ca81229124e90489fef49d72f779017981
Oleksandr Sydorenko <knsydorenko@gmail.com>
2021-03-26 21:52:23 -0400

    test

--- Codewars/formTheMinimum.js
+++ Codewars/formTheMinimum.js
@@ -1,4 +1,4 @@
-//Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates) adding a comment
+//Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates) adding a comment 1
 function minValue(values){
     return +values.filter((el,i,arr) => i === arr.lastIndexOf(el)).sort((a,b) => a - b).join('');
   }
